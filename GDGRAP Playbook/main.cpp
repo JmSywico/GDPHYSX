dd353de8488f3c7494ae26c5a2bbdb0c19bbd927
harookie <francis_m_obina@dlsu.edu.ph>
2025-02-19 11:49:25 +0800

    prog challenge 1

--- GDGRAP Playbook/main.cpp
+++ GDGRAP Playbook/main.cpp
@@ -1,19 +1,15 @@
+// main.cpp
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <GL/gl.h>
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <vector>
-
-#define TINYOBJLOADER_IMPLEMENTATION
-#include "tiny_obj_loader.h"
+#include "Model.h"
 
 float x_mod = 0;
 float y_mod = 0;
@@ -32,216 +28,163 @@ float axis_x = 0.0f;
 float axis_y = 1.0f;
 float axis_z = 0.0f;
 
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+float yaw = -90.0f;
+float pitch = 0.0f;
+float lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+std::vector<glm::vec3> modelPositions;
+float lastSpacePressTime = 0.0f;
+const float spaceCooldown = 3.0f;
+
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	if (action == GLFW_PRESS || action == GLFW_REPEAT)
-	{
-		if (key == GLFW_KEY_W)
-		{
-			y_mod = 0.0001f;
-		}
-		else if (key == GLFW_KEY_A)
-		{
-			x_mod = -0.0001f;
-		}
-		else if (key == GLFW_KEY_S)
-		{
-			y_mod = -0.0001f;
-		}
-		else if (key == GLFW_KEY_D)
-		{
-			x_mod = 0.0001f;
-		}
-		else if (key == GLFW_KEY_UP)
-		{
-			thetha -= 10;
-			axis_x = 1.0f;
-			axis_y = 0.0f;
-		}
-		else if (key == GLFW_KEY_DOWN)
-		{
-			thetha += 10;
-			axis_x = 1.0f;
-			axis_y = 0.0f;
-		}
-		else if (key == GLFW_KEY_LEFT)
-		{
-			thetha -= 10;
-			axis_x = 0.0f;
-			axis_y = 1.0f;
-		}
-		else if (key == GLFW_KEY_RIGHT)
-		{
-			thetha += 10;
-			axis_x = 0.0f;
-			axis_y = 1.0f;
-		}
-		else if (key == GLFW_KEY_Q)
-		{
-			scale_x -= 0.1f;
-			scale_y -= 0.1f;
-			scale_z -= 0.1f;
-		}
-		else if (key == GLFW_KEY_E)
-		{
-			scale_x += 0.1f;
-			scale_y += 0.1f;
-			scale_z += 0.1f;
-		}
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+    {
+        float currentTime = glfwGetTime();
+        if (currentTime - lastSpacePressTime >= spaceCooldown)
+        {
+            glm::vec3 newModelPos = cameraPos + cameraFront * 2.0f; // Place the new model 2 units in front of the camera
+            modelPositions.push_back(newModelPos);
+            lastSpacePressTime = currentTime;
+        }
+    }
 
-	}
-
-	if (action == GLFW_RELEASE)
-	{
-		if (key == GLFW_KEY_W || key == GLFW_KEY_S)
-		{
-			y_mod = 0.0f;
-		}
-		else if (key == GLFW_KEY_A || key == GLFW_KEY_D)
-		{
-			x_mod = 0.0f;
-		}
-	}
-
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, GLFW_TRUE);
-	}
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
 }
 
+void MouseCallback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
 
 int main(void)
 {
-	GLFWwindow* window;
+    GLFWwindow* window;
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "PC 1", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-	gladLoadGL();
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
 
-	glfwSetKeyCallback(window, KeyCallback);
+    glfwSetKeyCallback(window, KeyCallback);
+    glfwSetCursorPosCallback(window, MouseCallback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-	std::fstream vertSrc("Shaders/sample.vert");
-	std::stringstream vertBuff;
-	vertBuff << vertSrc.rdbuf();
-	std::string vertS = vertBuff.str();
-	const char* vert = vertS.c_str();
+    std::fstream vertSrc("Shaders/sample.vert");
+    std::stringstream vertBuff;
+    vertBuff << vertSrc.rdbuf();
+    std::string vertS = vertBuff.str();
+    const char* vert = vertS.c_str();
 
-	std::fstream fragSrc("Shaders/sample.frag");
-	std::stringstream fragBuff;
-	fragBuff << fragSrc.rdbuf();
-	std::string fragS = fragBuff.str();
-	const char* frag = fragS.c_str();
+    std::fstream fragSrc("Shaders/sample.frag");
+    std::stringstream fragBuff;
+    fragBuff << fragSrc.rdbuf();
+    std::string fragS = fragBuff.str();
+    const char* frag = fragS.c_str();
 
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vert, nullptr);
-	glCompileShader(vertexShader);
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vert, nullptr);
+    glCompileShader(vertexShader);
 
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &frag, nullptr);
-	glCompileShader(fragmentShader);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &frag, nullptr);
+    glCompileShader(fragmentShader);
 
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
 
-	glLinkProgram(shaderProgram);
+    glLinkProgram(shaderProgram);
 
-	std::string path = "3D/bunny.obj";
-	std::vector<tinyobj::shape_t> shapes;
-	std::vector<tinyobj::material_t> materials;
-	std::string warning, error;
+    Model model("3D/bunny.obj");
 
-	tinyobj::attrib_t attributes;
+    auto identity_matrix = glm::mat4(1.0f);
 
-	bool success = LoadObj(&attributes, &shapes, &materials, &warning, &error, path.c_str());
+    // Add the initial model position
+    modelPositions.push_back(glm::vec3(0.0f, 0.0f, 0.0f));
 
-	std::vector<GLuint> mesh_indices;
-	for (int i = 0; i < shapes[0].mesh.indices.size(); i++)
-	{
-		mesh_indices.push_back(shapes[0].mesh.indices[i].vertex_index);
-	}
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
-	constexpr int numVertices = 4; // Use a constant value for the array size
-	GLfloat vertices[numVertices * 3] = {
-		-0.5f, 0.5f, 0.f, // Top-left
-		0.5f, 0.5f, 0.f, // Top-right
-		0.5f, -0.5f, 0.f, // Bottom-right
-		-0.5f, -0.5f, 0.f // Bottom-left
-	};
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
 
-	GLuint indices[] = {
-		0, 1, 2, // First triangle
-		2, 3, 0 // Second triangle
-	};
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 640.0f / 480.0f, 0.1f, 100.0f);
 
-	GLuint VAO, VBO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+        for (const auto& pos : modelPositions)
+        {
+            glm::mat4 transformation_matrix = glm::translate(identity_matrix, pos);
+            transformation_matrix = glm::rotate(transformation_matrix, glm::radians(thetha), glm::vec3(axis_x, axis_y, axis_z));
+            transformation_matrix = glm::scale(transformation_matrix, glm::vec3(scale_x, scale_y, scale_z));
 
-	glBindVertexArray(VAO);
+            glm::mat4 mvp = projection * view * transformation_matrix;
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * attributes.vertices.size(), &attributes.vertices[0],
-	             GL_STATIC_DRAW);
+            model.Draw(shaderProgram, mvp);
+        }
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), nullptr);
-	glEnableVertexAttribArray(0);
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * mesh_indices.size(), mesh_indices.data(), GL_STATIC_DRAW);
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	auto identity_matrix = glm::mat4(1.0f);
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		// Update position based on modifiers
-		x += x_mod;
-		y += y_mod;
-
-		glm::mat4 transformation_matrix = translate(identity_matrix, glm::vec3(x, y, z));
-
-		transformation_matrix = rotate(transformation_matrix, glm::radians(thetha), glm::vec3(axis_x, axis_y, axis_z));
-		transformation_matrix = scale(transformation_matrix, glm::vec3(scale_x, scale_y, scale_z));
-
-		unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, value_ptr(transformation_matrix));
-
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-
-		glDrawElements(GL_TRIANGLES, mesh_indices.size(), GL_UNSIGNED_INT, nullptr);
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
-	}
-
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-
-	glfwTerminate();
-	return 0;
+    glfwTerminate();
+    return -1;
 }
+
